682f934e0511d3b3d2654f06752b0a42c5508d98
ajewe <ajewe@utexas.edu>
2019-12-11 15:44:17 -0600

    started assignment

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-hello
+let userData = {};
+let arrayOfUsers = [];
+
+window.onload = function() {
+  getUsers();
+}
+
+const getUsers = () => {
+  fetch('https://randomuser.me/api/?results=5000')
+    .then(res => res.json())
+    .then(users => userData = users)
+}
+
+const namePic = () => {
+  // const title = document.getElementById('title')
+
+  const allUsers = document.getElementById('all-users')
+
+  // while(allPosts.firstChild) {
+  //   allPosts.firstChild.remove()
+  // }
+
+  userData.results.map((users, index) => {
+    console.log(users)
+    const li = document.createElement('li')
+    const text = document.createTextNode(`#${index}, First Name: ${users.name.first}, Last Name:${users.name.last}`)
+    li.appendChild(text)
+    allUsers.append(li)
+  })
+}
