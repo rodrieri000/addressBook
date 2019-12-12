8d6ee53cd12dd0bf0b17497508650f5fb5adf4d7
ajewe <ajewe@utexas.edu>
2019-12-11 21:26:26 -0600

    added pics and more info

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.onload = function() {
 }
 
 const getUsers = () => {
-  fetch('https://randomuser.me/api/?results=5000')
+  fetch('https://randomuser.me/api/?results=500')
     .then(res => res.json())
     .then(users => userData = users)
 }
@@ -20,11 +20,28 @@ const namePic = () => {
   //   allPosts.firstChild.remove()
   // }
 
-  userData.results.map((users, index) => {
-    console.log(users)
+  userData.results.map((user, index) => {
+    console.log(user)
     const li = document.createElement('li')
-    const text = document.createTextNode(`#${index}, First Name: ${users.name.first}, Last Name:${users.name.last}`)
+    const pic = document.createElement('img')
+    const button = document.createElement('button')
+    button.innerHTML = 'More info'
+    button.onclick = () => {
+      moreInfo(user, li)
+      button.onclick = null
+    }
+    pic.src = user.picture.medium
+    const text = document.createTextNode(`#${index}, First Name: ${user.name.first}, Last Name: ${user.name.last} `)
+    
+    li.appendChild(button)
+    li.appendChild(pic)
     li.appendChild(text)
+    
     allUsers.append(li)
   })
+}
+
+const moreInfo = (user, li) => {
+  const text = document.createTextNode(`DOB: ${user.dob.date}, email: ${user.email}`)
+  li.appendChild(text)
 }
